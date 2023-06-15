703b5ce66a8f6c970e5af2ee252b00f992e6e401
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-06-15 10:30:30 +1000

    Update workout.js

--- js/workout.js
+++ js/workout.js
@@ -216,8 +216,6 @@ window.onload = async () => {
     //
     prevButton.style.display = "none";
     prevButtonDisabled.style.display = "flex";
-    //nextButton.click();
-    console.log("Click not working")
     //
   }
   else {
