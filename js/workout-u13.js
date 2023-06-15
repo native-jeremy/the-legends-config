a3ed60a26b5cbd52b5602a623358035547483563
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-06-15 13:39:46 +1000

    Update and rename workout-u12.js to workout-u13.js

--- js/workout-u13.js
+++ js/workout-u13.js
@@ -253,9 +253,7 @@ window.onload = async () => {
     prevButtonDisabled.style.display = "none";
   }
 
-  if (exerciseParam === undefined || exerciseParam === "undefined" && roundParam !== undefined || roundParam !== "undefined") {
-    setTimeout(nextPage, 1000);
-  }
+setTimeout(nextPage, 1000);
     
   function nextPage ()
   {
