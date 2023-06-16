df1f2011f4d2201bc19a40a0f50062185b0401f0
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-06-16 14:51:28 +1000

    Update and rename program-hub-u2.js to program-hub-u3.js

--- js/program-hub-u3.js
+++ js/program-hub-u3.js
@@ -32,7 +32,7 @@ window.onload = async () => {
   });
 }
 
-const circleProgress = new CircleProgress('.circle');
+const circleProgress = new CircleProgress('.app-completion-circle');
 circleProgress.attr({
 	max: 100,
 	value: 20,
