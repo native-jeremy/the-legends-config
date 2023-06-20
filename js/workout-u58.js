0984feeae7b63f26903b0a4a5c432ef77ab329c4
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-06-20 14:58:47 +1000

    Update and rename workout-u57.js to workout-u58.js

--- js/workout-u58.js
+++ js/workout-u58.js
@@ -74,7 +74,7 @@ window.onload = async () => {
     const repType = repDataInt.data[0].Rep_Type[0];
     console.log("Rep Type: ", repType);
 
-    const dataResponse = response.data;
+    const dataResponse = Wized.data.get("v.response");;
     console.log(dataResponse)
 
     let amrapBool = repDataInt.data; 
