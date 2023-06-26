4b1ae4be5e214346216453a76eb7cfc208055e24
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-06-26 12:18:50 +1000

    Update and rename workout-u287.js to workout-u288.js

--- js/workout-u288.js
+++ js/workout-u288.js
@@ -161,17 +161,17 @@ window.onload = async () => {
         timerText.innerHTML = counter + ".00";
         if (!timerText.classList.contains("pausetime")) {
           counter--;
-          if (counter < 2) {
+          if (counter < 0) {
+            playSiren();
             if (amrapBool === "True")
             { cookieIndex = amrapResponse.data[cookieIndex].Video.length
               Wized.data.setCookie("cookieindex", cookieIndex); // c.cookieindex"  
               const amrapIndex = Wized.data.get("c.cookieindex");
               console.log(amrapIndex)
             }
-          }
-          if (counter < 0) {
-            playSiren();
-            nextButton.click();
+            setTimeout(() => {
+              nextButton.click();
+            }, 1000)
             clearInterval(timer);
             clearInterval(checkAmrap);
             console.log("Completed");
