ad3e61f154dff26d28ed49c05aa973c28a9bc7c5
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-06-28 11:31:17 +1000

    Update and rename workout-u346.js to workout-u347.js

--- js/workout-u347.js
+++ js/workout-u347.js
@@ -354,7 +354,7 @@ window.onload = async () => {
       exerciseTitle.style.display = "flex";
       roundPopup.style.display = "none";
       roundText.style.display = "none";
-      setTimeout(autoPlayVideo, 2000);
+      //setTimeout(autoPlayVideo, 2000);
     }
 
     if (
