5be7c30bff9b00897b80be7d993a6f1306f38edd
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-06-29 22:45:21 +1000

    Update and rename workout-u396.js to workout-u397.js

--- js/workout-u397.js
+++ js/workout-u397.js
@@ -195,7 +195,7 @@ nextButton.addEventListener("click", function(){
   console.log("---------------------------------------");
   console.log("Video Array Length:", amrapResponse.data[parseInt(getExercisesNum)].Video.length);
   console.log("---------------------------------------");
-  console.log("Round Number", parseInt(roundParam), "Exercises Number", parseInt(getExercisesNum), "Exercise Number", parseInt(exerciseParam));
+  console.log("Round Number", parseInt(roundParam), "Exercises Number", parseInt(exercisesParam), "Exercise Number", parseInt(exerciseParam));
   });
 
   prevButton.addEventListener("click", function(){
