416e726f5986b3d30602e8a7602515bd9e4d339d
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-06-30 14:49:09 +1000

    Update and rename program-hub-u33.js to program-hub-u34.js

---
+++ js/program-hub-u34.js
@@ -0,0 +1,42 @@
+/* Define - Intialisation - Elements Retreived
+----------------------------------------------------------------
+*/
+
+//Element Triggers
+
+
+/* Wized Intialisation
+----------------------------------------------------------------
+*/
+
+window.onload = async () => {
+
+  // Completed Data Load
+  Wized.request.await("Load Users", (response) => {
+
+    if (response.status === 200)
+    {
+      const getData = response;
+      let applyData = getData.data.Completed;
+      console.log(applyData);
+      let workout = document.querySelectorAll(".workouts");
+      let icon = document.querySelectorAll(".completed-icon");
+
+      if (applyData.length !== 0)
+      {
+            for (let i = 0; i < applyData.length; i++) {
+              icon[i].classList.add("complete-indicator");
+              //console.log(applyData.length)
+            }
+            let progressNum = applyData.length / workout.length * 100
+            const circleProgress = new CircleProgress('.circle-latest');
+              circleProgress.attr({
+              max: 100,
+              value: progressNum,
+              textFormat: "percent",
+              indeterminateText: 0
+
+      }
+    }
+  });
+}
