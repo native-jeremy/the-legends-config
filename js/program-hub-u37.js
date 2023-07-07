7d592647e14061a2ead32686ede246223096aa2c
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-07-07 15:44:17 +1000

    Update and rename program-hub-u36.js to program-hub-u37.js

--- js/program-hub-u37.js
+++ js/program-hub-u37.js
@@ -17,14 +17,19 @@ window.onload = async () => {
     if (response.status === 200)
     {
       const getData = response;
-      let applyData = getData.data.Completed;
+      let applyData = getData.data;
       console.log(applyData);
       let workout = document.querySelectorAll(".workouts");
       let icon = document.querySelectorAll(".completed-icon");
+      let week = document.querySelectorAll(".week");
+      let workoutTitle = document.querySelectorAll(".workout-title");
 
       if (applyData !== undefined)
       {
         for (let i = 0; i < applyData.length; i++) {
+          if (week && workoutTitle === applyData) {
+            
+          }
             icon[i].classList.add("complete-indicator");
         }
         let progressNum = applyData.length / workout.length * 100
