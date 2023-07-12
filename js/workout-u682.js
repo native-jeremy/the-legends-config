e3d2e075f2aa96740d63843885a19cde10445c66
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-07-12 10:17:03 +1000

    Update and rename workout-u681.js to workout-u682.js

--- js/workout-u682.js
+++ js/workout-u682.js
@@ -535,7 +535,7 @@ window.onload = async () => {
       window.location.href = url.toString();      
     }
 
-    else if (window.location.href == "/workout") {
+    else if (window.location.href == "https://the-legends-web-app.webflow.io/workout") {
       roundPopup.style.display = "flex";
       roundText.style.display = "flex";
       RoundNumberText.innerHTML = "Redirecting..";
