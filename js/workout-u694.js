1e0b81591e35f43a8a91ac343b74778666f972a8
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-07-13 12:06:04 +1000

    Update and rename workout-u693.js to workout-u694.js

--- js/workout-u694.js
+++ js/workout-u694.js
@@ -559,7 +559,7 @@ window.onload = async () => {
       window.location.href = "/program-hub";
     }
     
-    else if (exerciseData == undefined && parseInt(exercisesParam) < 0 && parseInt(roundParam) === roundLength || parseInt(roundParam) !== roundLength){
+    else if (exerciseData == undefined && parseInt(roundParam) < 0){
       roundPopup.style.display = "flex";
       roundText.style.display = "flex";
       RoundNumberText.innerHTML = "Redirecting..";
