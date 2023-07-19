b606d9ffbe418311059603c2867b9e0e3af72973
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-07-19 14:45:41 +1000

    Update and rename questionnaire.js to questionnaire-u1.js

---
+++ js/questionnaire-u1.js
@@ -0,0 +1,54 @@
+window.onload = async () => {  
+    Wized.request.await("Load User", (response) => {
+      const userData = response;
+      console.log("User Data ", userData);
+
+      //Async function for grabbing the Questionnaire response
+       async function loadData() {
+          Wized.request.await("Load Questionnaire", (response) => {
+            const questionnaireData = response;
+            console.log("Questionnaire Data ", questionnaireData);
+        }
+        loadData();
+    })
+};
+
+$(document).ready(function() {
+  // get the anchor link buttons
+  const menuBtn = $('.button-style-4');
+  const menuBtn2 = $('.button-style-2');
+  const iconBtn = $('.form-indicator-icon');
+  // when each button is clicked
+  menuBtn.click(()=>{	
+    // set a short timeout before taking action
+    // so as to allow hash to be set
+    setTimeout(()=>{
+      // call removeHash function after set timeout
+      removeHash();
+    }, 0);
+  });
+  
+    menuBtn2.click(()=>{	
+    // set a short timeout before taking action
+    // so as to allow hash to be set
+    setTimeout(()=>{
+      // call removeHash function after set timeout
+      removeHash();
+    }, 0); // 5 millisecond timeout in this case
+  });
+  
+     iconBtn.click(()=>{	
+    // set a short timeout before taking action
+    // so as to allow hash to be set
+    setTimeout(()=>{
+      // call removeHash function after set timeout
+      removeHash();
+    }, 0);
+  });
+
+  // removeHash function
+  // uses HTML5 history API to manipulate the location bar
+  function removeHash(){
+    history.replaceState('', document.title, window.location.origin + 		     window.location.pathname + window.location.search);
+  }
+});
