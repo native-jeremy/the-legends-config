7e43b0b23cfeef31bb1987317bcb4717ba2a75c4
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-07-27 13:39:54 +1000

    Update and rename workout-u973.js to workout-u974.js

--- js/workout-u974.js
+++ js/workout-u974.js
@@ -395,7 +395,7 @@ window.onload = async () => {
                 controlMinusNumber.push(amrapMinus);
 
                 controlPlusNumber[i].addEventListener("click", () => {
-                  if (currentNumber + 1 < amrapMax) {
+                  if (currentNumber < amrapMax) {
                     currentNumber++;
                     videoSrcIndex[i] = currentNumber;
                     currentNumberText = videoSrcIndex[i] + 1
