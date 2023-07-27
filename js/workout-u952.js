2046fbf7ba59a799794c218b16e7f89af8e70693
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-07-27 11:56:53 +1000

    Update and rename workout-u951.js to workout-u952.js

--- js/workout-u952.js
+++ js/workout-u952.js
@@ -358,7 +358,7 @@ window.onload = async () => {
                 //Current Diffculty Text "1" - example
                 amrapCounter = document.createElement("div");
                 amrapCounter.classList.add("num", "current", "current-num");
-                amrapCounter.innerHTML = currentNumberText;
+                amrapCounter.innerHTML = currentNumber + 1;
 
                 amrapTrigger.appendChild(amrapCounter);
 
@@ -392,10 +392,10 @@ window.onload = async () => {
                 controlMinusNumber.push(amrapMinus);
 
                 controlPlusNumber[i].addEventListener("click", () => {
-                  if (currentNumber < ammrapLimitNumm) {
+                  if (currentNumber <= ammrapLimitNumm) {
                     currentNumber++;
                     videoSrcIndex[i] = currentNumber;
-                    currentNumberText = videoSrcIndex[i]
+                    currentNumberText = videoSrcIndex[i] + 1
                     //amrapTitle.innerHTML  = amrapCurrentNumm;
                     amrapCounter.innerHTML = currentNumberText;
                     //vidSrc.src = repDataInt.data[parseInt(exercisesParam)].Diff_Video[controlNumber[i]].url;
@@ -418,7 +418,7 @@ window.onload = async () => {
                   if (currentNumber > amrapMinNumm) {
                     currentNumber--;
                     videoSrcIndex[i] = currentNumber;
-                    currentNumberText = videoSrcIndex[i]
+                    currentNumberText = videoSrcIndex[i] + 1
                     //amrapTitle.innerHTML  = amrapCurrentNumm;
                     amrapCounter.innerHTML = currentNumberText;
                     if (videoIndex === videoSrcIndex[i])
