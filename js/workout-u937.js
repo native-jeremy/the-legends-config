2fe6e0b1b7fb2ea57be86caeb20b720e5cc99701
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-07-27 10:48:12 +1000

    Update and rename workout-u936.js to workout-u937.js

--- js/workout-u937.js
+++ js/workout-u937.js
@@ -257,62 +257,15 @@ window.onload = async () => {
             let currentNumberText;
 
             let controlNumber = [];
-            let num;
             
             if (controlNumber.length  <= 0) {
               
               for (let i = 0; i < amrapLength; i++)
               {
                 controlNumber.push(currentNumber);
-                /*vidSrc.src =
+                vidSrc.src =
                 exerciseDiffRes.data[parseInt(videoIndex)].Video[controlNumber[i]].url;
-                num = controlNumber[i];*/
               }
-
-              checkAmrapVideo = setInterval(() => {
-                  
-                let videoCurrentSrc;
-                let videos = document.getElementById("video");
-      
-                if (Math.floor(videos.currentTime) === Math.floor(videos.duration)) {
-                  if (
-                    videoIndex < diffLength
-                  ) {
-                    videoIndex = videoIndex + 1;
-                    console.log("Current Exercise Index For Video", parseInt(videoIndex));
-      
-                    videoCurrentSrc =
-                    diffRes.data[parseInt(videoIndex)].Video[
-                      num
-                      ].url;
-                    console.log("---------------------------------------");
-                    console.log(videoCurrentSrc);
-                    console.log("---------------------------------------");
-                    console.log("Ran Request Video Src => Updated");
-      
-                    videos.src = videoCurrentSrc;
-                    videos.play();
-                  }
-                } else if (
-                  videoIndex >= diffLength
-                ) {
-                  videoIndex = 0;
-                  videoCurrentSrc =
-                  exerciseDiffRes.data[parseInt(videoIndex)].Video[
-                    num
-                    ].url;
-                  console.log("---------------------------------------");
-                  console.log(videoCurrentSrc);
-                  console.log("---------------------------------------");
-                  console.log("Ran Request Video Src <= Reset ");
-      
-                  console.log("---------------------------------------");
-                  console.log("Current Video Index:", parseInt(videoIndex));
-      
-                  videos.src = videoCurrentSrc;
-                  videos.play();
-                }
-              }, 0);
             }
 
             if (controlNumber.length > 0) {
@@ -468,6 +421,51 @@ window.onload = async () => {
                     console.log("CurrentNumber Array: ", controlNumber);
                   }
                 });
+
+                checkAmrapVideo = setInterval(() => {
+                  
+                  let videoCurrentSrc;
+                  let videos = document.getElementById("video");
+        
+                  if (Math.floor(videos.currentTime) === Math.floor(videos.duration)) {
+                    if (
+                      videoIndex < diffLength
+                    ) {
+                      videoIndex = videoIndex + 1;
+                      console.log("Current Exercise Index For Video", parseInt(videoIndex));
+        
+                      videoCurrentSrc =
+                      diffRes.data[parseInt(videoIndex)].Video[
+                        controlNumber[i]
+                        ].url;
+                      console.log("---------------------------------------");
+                      console.log(videoCurrentSrc);
+                      console.log("---------------------------------------");
+                      console.log("Ran Request Video Src => Updated");
+        
+                      videos.src = videoCurrentSrc;
+                      videos.play();
+                    }
+                  } else if (
+                    videoIndex >= diffLength
+                  ) {
+                    videoIndex = 0;
+                    videoCurrentSrc =
+                    exerciseDiffRes.data[parseInt(videoIndex)].Video[
+                      controlNumber[i]
+                      ].url;
+                    console.log("---------------------------------------");
+                    console.log(videoCurrentSrc);
+                    console.log("---------------------------------------");
+                    console.log("Ran Request Video Src <= Reset ");
+        
+                    console.log("---------------------------------------");
+                    console.log("Current Video Index:", parseInt(videoIndex));
+        
+                    videos.src = videoCurrentSrc;
+                    videos.play();
+                  }
+                }, 0);
               }
             }
 
