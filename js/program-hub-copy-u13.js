97cb0bf2bc724cd05955d5b2823263e4d2ab6a0a
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-08-01 20:55:41 +1000

    Update and rename program-hub-copy-u12.js to program-hub-copy-u13.js

---
+++ js/program-hub-copy-u13.js
@@ -0,0 +1,49 @@
+/* Define - Intialisation - Elements Retreived
+----------------------------------------------------------------
+*/
+
+//Element Triggers
+
+
+/* Wized Intialisation
+----------------------------------------------------------------
+*/
+
+window.onload = async () => {
+
+    // Completed Data Load
+    Wized.request.await("Load Users Program Hub", (response) => {
+        const getData = response;
+        let applyCompleted = getData.data.Completed;
+        let applyWeek = getData.data.Completed_Week;
+        let applyName = getData.data.Completed_Name;
+        let applyWorkout = getData.data.Completed_Workout_ID;
+        console.log('getData ', getData)
+  
+        const app = Vue.createApp( {
+        data () {
+          return {
+              showBooks: true,
+              books: [
+                  {title: "Hello MJX Family", author: 'Morgan', image: 'images/mjx.jpg'},
+                  {title: "Hello Xavier", author: 'Morgan', image: 'images/xav.jpg'},
+                  {title: "Hello Jessica", author: 'Morgan', image: 'images/jess.jpg'},
+              ],
+          }
+        },
+        methods: {
+            toggleBooks() {
+                this.showBooks = !this.showBooks
+            },
+        }
+        })
+  
+        app.mount('#app');
+          const circleProgress = new CircleProgress('.circle-latest');
+          circleProgress.attr({
+          max: 100,
+          value: 0,
+          textFormat: "percent",
+          indeterminateText: 0});
+    });
+  };
