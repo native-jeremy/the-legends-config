dac46fe19079b95a9d18f1d3ff8edaace0139fd7
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-08-02 16:18:22 +1000

    Update and rename program-hub-vanilla-31.js to program-hub-vanilla-32.js

--- js/program-hub-vanilla-32.js
+++ js/program-hub-vanilla-32.js
@@ -14,7 +14,7 @@ window.onload = async () => {
 
      // Completed Data Load
     Wized.request.await("Load Users Program Hub", (response) => {
-       console.log("Users ", response);
+       console.log("Users ", response.data.Add_Program[0]);
 
        //let users = response.data.Add_Program[0];
 
