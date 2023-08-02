9bc8230d33c90ea3a4e57d18050d676b7901b1b3
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-08-02 16:59:43 +1000

    Update and rename program-hub-vanilla-47.js to program-hub-vanilla-48.js

--- js/program-hub-vanilla-48.js
+++ js/program-hub-vanilla-48.js
@@ -37,6 +37,7 @@ window.onload = async () => {
 
        for (let k = 0; k < diffRes.length; k++) {
         const programSelected = diffRes.filter(checkDiffProgram);
+        console.log("Users Program Selected ", programSelected);
        };
 
        function checkDiffProgram(diffProgram)
@@ -80,7 +81,6 @@ window.onload = async () => {
 
         console.log("Weeks " + i + " Filtered Array", weeks);
         console.log("Users Program Array ", program);
-        console.log("Users Program Selected ", programSelected);
        }
       });
      }
