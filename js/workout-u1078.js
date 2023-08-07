8d1d396159665fbf01d99cc4393b327927811df9
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-08-07 14:39:30 +1000

    Update and rename workout-u1077.js to workout-u1078.js

--- js/workout-u1078.js
+++ js/workout-u1078.js
@@ -281,7 +281,8 @@ window.onload = async () => {
                   repDataInt.data[
                     parseInt(exercisesParam)
                   ].Diff_Exercise_Lookup.reverse();*/
-                let sortedAmrapTitle = exerciseDiffRes.data[i].Exercise_Name;
+                let reverse = exerciseDiffRes.reverse();
+                let sortedAmrapTitle = reverse.data[i].Exercise_Name;
                 let amrapResTitle = sortedAmrapTitle;
                 console.log("Data", sortedAmrapTitle);
                 console.log("Title", amrapResTitle);
