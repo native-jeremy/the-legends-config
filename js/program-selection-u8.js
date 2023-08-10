70f429c715e90c9ca8725058be9720d7c512246d
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-08-10 10:14:53 +1000

    Update and rename program-selection-u7.js to program-selection-u8.js

--- js/program-selection-u8.js
+++ js/program-selection-u8.js
@@ -225,10 +225,9 @@ window.onload = async () => {
               },
               });
               /////////////////////////////////////////
+              triggerLoader.click();
               swiper.on("slideChange", function () {
               Webflow.require("ix2").init();
-
-              triggerLoader.click();
               /*slideBtn_Left.classList.remove("swiper-button-lock");
               slideBtn_Right.classList.remove("swiper-button-lock");*/
               });
