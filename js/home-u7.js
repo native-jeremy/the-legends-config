730ba4966643077f561f1d5b68fd26cbd6ebcc3c
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-08-19 14:50:35 +1000

    Update and rename home-u6.js to home-u7.js

--- js/home-u7.js
+++ js/home-u7.js
@@ -129,7 +129,7 @@ window.onload = async () => {
     console.log("Programs Length", snapshot.Programs.length);
 
     //Programs Render List
-    for (let i = 0; i < snapshot.Programs.length; i++) {
+    /*for (let i = 0; i < snapshot.Programs.length; i++) {
       const card = `
       <div id="w-node-a5574579-43de-e56f-650f-7731fc6fc9dd-061c0e03" class="card scroll_card">
         <div class="card_image">
@@ -142,7 +142,7 @@ window.onload = async () => {
         </div>
       </div>`;
       programContent.innerHTML = programContent.innerHTML + card;
-    }
+    }*/
 
     //Custom Slider Dots || Arrows || Elements / Event Listener Applied
     //Webflow Arrows | Buttons
