e9fd06e7d81eef9350459b15ba8bc7e80638e001
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-09-15 14:11:56 +1000

    Update and rename isUserVerified.js to isUserVerified-v1.js

--- js/member/isUserVerified.js
+++
@@ -1,8 +0,0 @@
-window.onload = async () => {
-  Wized.request.await("Load Users", (response) => {
-    const snapshot = response.data;
-    if(snapshot.Stripe == "Not Verified") {
-      window.location.href = "/program-selection";
-    }
-  });
-};
