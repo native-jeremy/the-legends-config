dc47b7c8bf653988b992019ca1a942dff9c39915
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-10-06 15:53:15 +1100

    Create wizedTest.js

---
+++ js/test/wizedTest.js
@@ -0,0 +1,2 @@
+{{r.63.d[v.index].Rep_Type_Linked_Exercises[v.secondindex] == "Time" ?
+r.63.d[v.index].Amounts_Name_Linked_Exercises[v.secondindex] + " " + "seconds" : r.63.d[v.index].Amounts_Name_Linked_Exercises[v.secondindex] + " " + "reps"}}
