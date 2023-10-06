66bd866ba12e0839e17ca3aca23dd8bf51057ee6
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-10-06 15:46:55 +1100

    Update and rename workout-overview-u23.js to workout-overview-u24.js

--- js/member/workout-overview-u24.js
+++ js/member/workout-overview-u24.js
@@ -45,35 +45,33 @@ window.onload = async () => {
             currentRound.Rep_Type_Linked_Exercises.forEach((amount, index) => {
                 if(amount == "Time")
                 {
-                    setTimeout(() => {
-                        const timeConvert = document.querySelectorAll('.convert_time');
-                        timeConvert.forEach(time => {
-                         if(parseInt(time.textContent) < 60)
-                         {   
-                            time.textContent = time.textContent + " seconds";
-                         }
-                         else {
-                             let timer = parseInt(time.textContent)
-                             let = convertedTime = Math.floor(timer/ 60)
-                             let extraSeconds = timer % 60;
-                             if(!extraSeconds > 0)
-                             {
-                                 time.textContent = convertedTime + " minutes";
-                             }
-                             else {
-                                 time.textContent = convertedTime + " minutes " + extraSeconds + " seconds";
-                             }
-                         }
-                        });
-                     }, 1000);
+                    const timeConvert = document.querySelectorAll('.convert_time')[index];
+                    timeConvert.forEach(time => {
+                    if(parseInt(time.textContent) < 60)
+                    {   
+                        time.textContent = time.textContent + " seconds";
+                    }
+                    else {
+                    let timer = parseInt(time.textContent)
+                    let = convertedTime = Math.floor(timer/ 60)
+                    let extraSeconds = timer % 60;
+                    if(!extraSeconds > 0)
+                    {
+                        time.textContent = convertedTime + " minutes";
+                    }
+                    else {
+                        time.textContent = convertedTime + " minutes " + extraSeconds + " seconds";
+                    }
+                    }
+                    });
                 }
                 else if (amount == "Rep") {
+                    const time = document.querySelectorAll('.convert_time')[index];
                     time.textContent = time.textContent + " Reps";
                 }
             });
         });
-        console.log(response); // Log request response  
-    })
+    });
 
   // Siren & Voice Functionailty Setup
   const sirenCookieInt = await Wized.data.get("c.sirenmute");
