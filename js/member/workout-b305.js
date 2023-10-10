4038213b0bda6a8fc402eb47629d1fa2bc3d0b8d
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-10-10 12:49:52 +1100

    Update and rename workout-b304.js to workout-b305.js

--- js/member/workout-b305.js
+++ js/member/workout-b305.js
@@ -461,14 +461,14 @@ window.onload = async () => {
                     srcIndex++;
                     if (srcIndex < amrapLength) {
                       //videoCurrentSrc = secondaryResponse.data[videoIndex].Video[currentNumber].url;
-                      vidSrc.src = secondaryResponse.data[srcIndex].Video[videoSrcIndex[srcIndex]].url;
+                      vidSrc.src = secondaryResponse.data[srcIndex].Video[0].url;
                       vidSrc.play();
                     }
                   } else
                     {
                         srcIndex = 0;
                         //videoCurrentSrc = secondaryResponse.data[videoIndex].Video[currentNumber].url;
-                        vidSrc.src = secondaryResponse.data[srcIndex].Video[videoSrcIndex[srcIndex]].url;
+                        vidSrc.src = secondaryResponse.data[srcIndex].Video[0].url;
                         vidSrc.play();
                     }
                 }, 1000);
