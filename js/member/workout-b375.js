cfeb1a77241f49e9b3e7f085eabd53788916f473
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-10-16 15:24:06 +1100

    Update and rename workout-b374.js to workout-b375.js

--- js/member/workout-b375.js
+++ js/member/workout-b375.js
@@ -152,20 +152,6 @@ window.onload = async () => {
     audioRes = response;
   })
 
-    let recoveryData = Wized.data.get("v.complete");
-    //let getType = checkurl.get("type");
-    //if(getType == "recovery")
-    if(recoveryData == "completed")
-    {
-      recoveryMessage.style.display = "flex";
-      workoutMessage.style.display = "none";
-    }
-    else  {
-      recoveryMessage.style.display = "none";
-      //recoveryLink.href = recoveryLink.href + `?workout=${recQxMVw9YOyffPfi}&round=0&exercises=0&exercise=0&esi=notset&type=recovery`
-      workoutMessage.style.display = "flex";
-    }
-
   Wized.request.await("Load Round Info", (response, exerciseDiffRes) => {
     const mainResponse = response;
     const repDataInt = response;
@@ -236,6 +222,20 @@ window.onload = async () => {
       RoundNumberText.innerHTML = "Warm Up";
       roundTitle.innerHTML = "";
       roundNumHeader.innerHTML = "";
+    }
+
+    let recoveryData = Wized.data.get("v.complete");
+    //let getType = checkurl.get("type");
+    //if(getType == "recovery")
+    if(recoveryData == "completed")
+    {
+      recoveryMessage.style.display = "flex";
+      workoutMessage.style.display = "none";
+    }
+    else  {
+      recoveryMessage.style.display = "none";
+      //recoveryLink.href = recoveryLink.href + `?workout=${recQxMVw9YOyffPfi}&round=0&exercises=0&exercise=0&esi=notset&type=recovery`
+      workoutMessage.style.display = "flex";
     }
 
       //----------------------------------------------------------------
