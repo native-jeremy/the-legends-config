b353a3b6f8e08b9352b6e26f5e6edd0ab1462ea4
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-10-18 15:45:50 +1100

    Update and rename recovery-v30.js to recovery-v31.js

--- js/member/recovery-v31.js
+++ js/member/recovery-v31.js
@@ -15,7 +15,6 @@ const timerText = document.getElementById("safeTimerDisplay");
 const currentTest = document.getElementById("current");
 const durationTest = document.getElementById("dur");
 const RoundNumberText = document.getElementById("mainNumText");
-const RoundSmall = document.getElementById("roundSmallText");
 const progressEl = document.querySelector(".wheel");
 let loader = document.getElementById("loader");
 const returnMessage = document.getElementById("return");
@@ -120,10 +119,9 @@ window.onload = async () => {
       roundText.style.display = "flex";
     }
     else {
-      RoundNumberText.textContent = "Recovery";
-      RoundSmall.textContent = ""
-      roundTitle.textContent = "Let's begin your";
-      roundNumHeader.textContent = "Recovery";
+      RoundNumberText.innerHTML = "Recovery";
+      roundTitle.innerHTML = "Let's begin your";
+      roundNumHeader.innerHTML = "";
     }
   }
 
