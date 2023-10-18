71db953d4c518241c9aa1382fd295802cfcf6fd8
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-10-18 15:49:54 +1100

    Update and rename recovery-v33.js to recovery-v34.js

--- js/member/recovery-v34.js
+++ js/member/recovery-v34.js
@@ -306,7 +306,6 @@ window.onload = async () => {
 
       function exitParams() {
         workoutExitButton.href = "/recovery-overview?recovery=" + recoveryParam;
-        localStorage.removeItem("recovery");
       }
 
       function updateParams() {
