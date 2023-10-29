a9978174924b206751148856cfdc241f6e2c2e1a
native-jeremy <106562450+native-jeremy@users.noreply.github.com>
2023-10-30 10:46:40 +1100

    Update and rename workout-b487.js to workout-b488.js

--- js/member/workout-b488.js
+++ js/member/workout-b488.js
@@ -517,7 +517,11 @@ window.onload = async () => {
                       vidSrc.src = secondaryResponse.data[srcIndex].Video[videoSrcIndex[srcIndex] - 1].url;
                      //setTimeout(function () {      
                         vidSrc.load();
-                        vidSrc.play();
+                        if(vidSrc.paused)
+                        {
+                          vidSrc.play();
+                        }
+                       
                     // }, 1);
                     }
                     else if (srcIndex >= amrapLength)
@@ -527,7 +531,10 @@ window.onload = async () => {
                         vidSrc.src = secondaryResponse.data[srcIndex].Video[videoSrcIndex[srcIndex] - 1].url;
                         //setTimeout(function () {      
                           vidSrc.load();
-                          vidSrc.play();
+                          if(vidSrc.paused)
+                          {
+                            vidSrc.play();
+                          }
                        //}, 1);
                     }
                 }
